3a26ae4d1697cd70ca0cf18d7c1dc6eef54612fe
notminusone <58552347+notminusone@users.noreply.github.com>
2022-10-19 21:13:51 +0800

    fix

--- app.py
+++ app.py
@@ -81,12 +81,20 @@ def part12():
 	if request.method=='GET':
 		return render_template('part12.html',part12_active = "active",title="Part 12")
 	if request.method=='POST':
-		latitude = request.form["latitude"]
-		longitude = request.form["longitude"]
+		latitude1 = request.form["latitude1"]
+		latitude2 = request.form["latitude2"]
+		low_latitude = min(latitude1,latitude2)
+		high_latitude = max(latitude1,latitude2)
+	
+		longitude1 = request.form["longitude1"]
+		longitude2 = request.form["longitude2"]
+		low_longitude = min(longitude1,longitude2)
+		high_longitude = max(longitude1,longitude2)
 		cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:notminusone.database.windows.net,1433;Database=notminusoneDatabase;Uid=not-1;Pwd={0626Fuyi};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 		cursor = cnxn.cursor()
 		# cursor.execute("select id,latitude,longitude,net,place from nquakes2 where latitude=?",latitude," and longitude=?",longitude)
-		cursor.execute("select id,latitude,longitude,net,place from nquakes2 where latitude=35.051 and longitude=-117.6728333")
+		cursor.execute("select id,latitude,longitude,net,place from nquakes2 where latitude between"+low_latitude+" and "+high_latitude+
+		" and longitude between"+low_longitude+" and "+high_longitude)
 		row = cursor.fetchall()
 		if row is not None:
 			return render_template('part12.html',part12_active = "active",data =row)
@@ -123,10 +131,6 @@ def part13():
 
 @app.route('/delete',methods=['POST'])
 def delete():
-	id = request.button['id']
-	cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:notminusone.database.windows.net,1433;Database=notminusoneDatabase;Uid=not-1;Pwd={0626Fuyi};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-	cursor = cnxn.cursor()
-	cursor.execute("delete from nquakes where id=?",id)
 	return render_template('part13.html',part13_active = "active",title="Part 13")
 
 @app.route('/edit',methods=['POST'])
